f5809222004fac4f2b2ddcb931d667c6c7f5a455
yago santos <yagolcs@msn.com>
2020-02-18 14:27:03 -0300

    Create CRUD for tasks.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  resources :tasks
+  # get 'tasks', to: 'tasks#index'
+  # get 'tasks/new', to: 'tasks#new', as: :new
+  # post 'tasks', to: 'tasks#create'
+  # get 'tasks/:id/edit', to: 'tasks#edit', as: :edit
+  # patch 'tasks/:id', to: 'tasks#update', as: :update
+  # get 'tasks/:id', to: 'tasks#show', as: :task
+  # delete 'tasks/:id', to: 'tasks#destroy', as: :delete
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
